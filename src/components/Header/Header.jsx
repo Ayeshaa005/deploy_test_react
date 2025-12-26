2faf53fda08939ef25f60960ae5bd66aaeec8780
sidrajamil <sidrajamil152008@gmail.com>
2025-12-26 15:17:19 +0500

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+
+
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className='bg-purple-600 font-bold text-3xl text-white mt-[30px] text-center'>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi pariatur atque, doloribus provident ipsum aspernatur laboriosam porro itaque excepturi unde, voluptatum eum odio repudiandae culpa est. Rem consequuntur mollitia inventore.</p>
+    </div>
+
+
+     
+  )
+}
+
+export default Header
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // <-- make sure this includes your files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
